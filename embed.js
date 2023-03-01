0675d9482f0bcf2fd48422cceb822994d7a156bf
CHUCKIE525 <83316107+CHUCKIE525@users.noreply.github.com>
2023-03-01 09:55:44 -0700

    More embed code

--- embed.js
+++ embed.js
@@ -1,9 +1,20 @@
+$(document).ready(function(){
+  $('#sendUrl').click(function(){
+    function getId(url) {
+    var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+    var match = url.match(regExp);
 
-function embed()
-{
-    var url = document.getElementById('url').value 
-		document.getElementById('myframe').src = url;
-
+    if (match && match[2].length == 11) {
+        return match[2];
+    } else {
+        return 'error';
+    }
 }
+var videoId = $('#url').val();
+var myId = getId(videoId);
 
-document.getElementById("mybutton").addEventListener("click", embed);
+$('#videoSrc').html(myId);
+
+$('#myVideo').html('<iframe src="//www.youtube.com/embed/' + myId + '" frameborder="0" allowfullscreen></iframe>');
+  })
+});
